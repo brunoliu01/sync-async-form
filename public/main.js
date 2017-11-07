f6164fd3bbb19e80edabf61cf8b07ca2c9c26bb9
Alumno13 J601 <a13j601@campustls.local>
2017-11-07 12:46:47 -0500

    sigo avanzando formulario

--- public/main.js
+++ public/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function (){
-
     $('input[type="submit"]').on("click", function(event) {
-        var first_name = $('input[name="first_name"]').val();
-        console.log(first_name);
-
         event.preventDefault();
+
+        var first_name = $('#first_name').val();
+
+       /* $.ajax('find.html' , { 
+        	method: 'post',
+        	data: {
+        		'first_name': first_name
+        	}
+    	});*/
+    	$.get('find.html' ,  { 'first_name': first_name});
+    	$.post('find.html' ,  { 'first_name': first_name});
     });
-})
+});
+
