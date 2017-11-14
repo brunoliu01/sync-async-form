567aa1aa94e5b768987226f83aba1c4dbd610322
Alumno12 J601 <a12j601@campustls.local>
2017-11-14 12:10:31 -0500

    avance

--- public/main.js
+++ public/main.js
@@ -1,17 +1,35 @@
 $(document).ready(function (){
-    $('input[type="submit"]').on("click", function(event) {
-        event.preventDefault();
+  $('input[type="submit"]').on("click", function(event) {
+      event.preventDefault();
 
-        var first_name = $('#first_name').val();
+     /* $.ajax('find.html' , {
+          method: 'post',
+          data: {
+              'first_name': first_name
+          }
+      });*/
 
-       /* $.ajax('find.html' , { 
-        	method: 'post',
-        	data: {
-        		'first_name': first_name
-        	}
-    	});*/
-    	$.get('find.html' ,  { 'first_name': first_name});
-    	$.post('find.html' ,  { 'first_name': first_name});
+      var first_name = $('#first_name').val(); //array [1]
+      var last_name = $('#last_name').val(); // val()
+      var gender = $('input[name="gender"]:checked').val();//val ejecuta el valor de la primera coincidencia
+      var interest = $('input[name="interest"]:checked');
+      var interests = new Array();
+
+
+      for (var i = 0; i < interest.length; i++) {
+        var temp_interest = interest[i];
+        interests.push(temp_interest.value);
+      }
+
+      var payload = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'gender': gender,
+        'interests': interests
+      }
+      
+      $.post("find.html" , payload);
+    
     });
-});
 
+  });
